5356e96a7c0a1be8856a1400fb3de09732867338
emarvelm <marvelousegbe19@gmail.com>
2024-04-21 07:43:17 +0100

    starts a flask application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/home/marvel/wurd/pythonLearning/Flask/web_flask/bin/python3
+""" starts a flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello HBNB!"
+
+app.run()
