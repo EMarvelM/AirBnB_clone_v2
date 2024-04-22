f2785279e845ec80a6f38df4e0c66b01af5e455e
emarvelm <marvelousegbe19@gmail.com>
2024-04-22 20:12:09 +0100

    grant select priviledge to hbnb_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,12 +6,12 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Granting the SELECT privilege for the user hbnb_test on the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
 
 -- Granting all privileges to the new user on hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
 
