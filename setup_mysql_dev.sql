b220e6ff0f4c80748d885cac63cc9b75dfa47707
emarvelm <marvelousegbe19@gmail.com>
2024-04-22 20:09:37 +0100

    hbnb_dev user instead of hbnb_test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Creating new user named: hbnb_test with all privileges on the db hbnb_dev_db
 -- with the password: hbnb_test_pwd if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Granting the SELECT privilege for the user hbnb_test on the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
